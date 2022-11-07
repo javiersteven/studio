5cb6c8ebc2ba916b7c1bdf3d1cb4fa183bd4a191
Javier Barrera <javiersteven503@gmail.com>
2022-11-07 17:54:05 -0500

    fix: path of the icon's

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -6,10 +6,10 @@ const Banner = () => {
       <h1 className="text-3xl font-bold">Javier Barrera</h1>
       <p className="text-2xl font-semibold">Desarrollador FrontEnd Jr.</p>
       <div className="flex flex-row gap-x-6 mt-3">
-        <Icon path="src/assets/html5.svg" language="HTML" width="38px"/>
-        <Icon path="src/assets/js.svg" language="JavaScript" />
-        <Icon path="src/assets/css3-alt.svg" language="CSS" width="38px"/>
-        <Icon path="src/assets/python.svg" language="Python" />
+        <Icon path="../assets/html5.svg" language="HTML" width="38px"/>
+        <Icon path="../assets/js.svg" language="JavaScript" />
+        <Icon path="../assets/css3-alt.svg" language="CSS" width="38px"/>
+        <Icon path="../assets/python.svg" language="Python" />
       </div>
     </div>
   )
